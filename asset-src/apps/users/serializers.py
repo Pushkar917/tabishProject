7dd630ede2d6288c67e739de777e496887fce23a
Pushkar917 <pshkranand40@gmail.com>
2022-10-20 13:07:14 +0530

    feat: adding custom user

---
+++ asset-src/apps/users/serializers.py
@@ -0,0 +1,19 @@
+'''from django.contrib.auth import get_user_model
+from djoser.serializers import UserCreateSerializer
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model = User
+        fields = ["id", "username", "email", "first_name", "last_name", "password"]
+
+
+        '''
