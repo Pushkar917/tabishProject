e24664b23d8f4322605c3ae4720aef1a1027b8ea
Pushkar917 <pshkranand40@gmail.com>
2022-10-20 13:07:31 +0530

    Adding Custom User

---
+++ asset-src/asset-app/urls.py
@@ -0,0 +1,32 @@
+"""assetApp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('supersecret/', admin.site.urls),
+    #path("api/v1/auth/", include("djoser.urls")),
+    #path("api/v1/auth/", include("djoser.urls.jwt")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+admin.site.site_header = "Real Estate Admin"
+admin.site.site_title = "Real Estate Admin Portal"
+admin.site.index_title = "Welcome to real estate adminstration"
